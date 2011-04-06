a4095095d7e69d66aebc62433959845132f471fb
Brian Lamar <brian.lamar@rackspace.com>
2011-04-05 21:24:47 -0400

    First working test.

--- src/microcore/opt/bootlocal.sh
+++ src/microcore/opt/bootlocal.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 # put other system startup commands here
+ifconfig eth0 add 169.254.10.1/24
+ifconfig eth0 up
+/usr/sbin/udhcpd -f /usr/local/etc/udhcpd.conf &
+/etc/init.d/services/tftpd start
